f93f78cc6548960baba616515b02251155787b0a
SamuelWhitton <sampwhitton@gmail.com>
2020-04-10 10:10:49 +1000

    .

--- TestFiles/run_daina_compilation_test.sh
+++ TestFiles/run_daina_compilation_test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-daina -output DainaCompiler_Build/Daina -compile $(find $(pwd)/DainaCompiler -type f -not -path '*/\.*' |grep '.dai$')
+./DainaCompiler_Build/Daina < TestFiles/daina_compilation_script.dai-compilation
+#daina -output DainaCompiler_Build/Daina -compile $(find $(pwd)/DainaCompiler -type f -not -path '*/\.*' |grep '.dai$')
