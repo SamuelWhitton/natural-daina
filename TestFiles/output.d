7bfe755d7e91c76ac4b3b42f4c15c4e8261d28a3
Samuel Whitton <whittonsamuel@gmail.com>
2025-01-19 17:50:37 +1100

    Stage one bootstrapped compiler ...

--- TestFiles/output.d
+++ TestFiles/output.d
@@ -14,25 +14,30 @@ class ENTRY_POINT_CLASS {
 
 /* *** CLASS: Object *** */
 interface CLASSTYPE_Object : OBJECT, LAMBDA {
+   OBJECT IMETHOD_asdf(OBJECT caller, OBJECT[] parameters);
 }
 class CLASSIMP_Object : CLASSTYPE_Object {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
+   override OBJECT IMETHOD_asdf(OBJECT caller, OBJECT[] parameters) {
+      return caller;
+   }
 }
 
 /* *** CLASS: DainaCommandParser *** */
 interface CLASSTYPE_DainaCommandParser : OBJECT, LAMBDA, CLASSTYPE_Object {
-   OBJECT parseNextCommandAndSendToReceiverWithCommandStringSegmentProvider(OBJECT caller, OBJECT[] parameters);
-   OBJECT getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters);
+   OBJECT IMETHOD_parseNextCommandAndSendToReceiverWithCommandStringSegmentProvider(OBJECT caller, OBJECT[] parameters);
+   OBJECT IMETHOD_getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters);
 }
 class CLASSIMP_DainaCommandParser : CLASSTYPE_DainaCommandParser {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
-   static OBJECT newDainaCommandParser(OBJECT caller, OBJECT[] parameters) {
+   private OBJECT IOBJECT_validDainaCommandsToParse = null;
+   static OBJECT CMETHOD_newDainaCommandParser(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   override OBJECT parseNextCommandAndSendToReceiverWithCommandStringSegmentProvider(OBJECT caller, OBJECT[] parameters) {
+   override OBJECT IMETHOD_parseNextCommandAndSendToReceiverWithCommandStringSegmentProvider(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   override OBJECT getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters) {
+   override OBJECT IMETHOD_getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
 }
@@ -42,22 +47,22 @@ interface CLASSTYPE_DainaCommandStrings : OBJECT, LAMBDA {
 }
 class CLASSIMP_DainaCommandStrings : CLASSTYPE_DainaCommandStrings {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
-   static OBJECT PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_AND_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_AND_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT PARSE_DAINA_SOURCE_FILE_IN_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT ANALYSE_AND_SYNTHESISE_SYNTAX_TREE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_ANALYSE_AND_SYNTHESISE_SYNTAX_TREE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT COMPILE_INTO_JAVA_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_COMPILE_INTO_JAVA_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT COMPILE_INTO_C_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_COMPILE_INTO_C_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
 }
@@ -67,79 +72,82 @@ interface CLASSTYPE_DainaCommandDescriptionStrings : OBJECT, LAMBDA {
 }
 class CLASSIMP_DainaCommandDescriptionStrings : CLASSTYPE_DainaCommandDescriptionStrings {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
-   static OBJECT PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_AND_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_AND_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT PARSE_DAINA_SOURCE_FILE_IN_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_PRIVATE_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT ANALYSE_AND_SYNTHESISE_SYNTAX_TREE(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_ANALYSE_AND_SYNTHESISE_SYNTAX_TREE(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT COMPILE_INTO_JAVA_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_COMPILE_INTO_JAVA_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT COMPILE_INTO_C_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT TMETHOD_COMPILE_INTO_C_WITHIN_DIRECTORY(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
 }
 
 /* *** CLASS: ValidDainaCommandsToParse *** */
 interface CLASSTYPE_ValidDainaCommandsToParse : OBJECT, LAMBDA, CLASSTYPE_Object {
-   OBJECT getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters);
-   OBJECT maybeParsingActionForCommandStringIfValidDainaCommand(OBJECT caller, OBJECT[] parameters);
-   OBJECT addValidDainaCommandForCommandStringAndParsingActionAndDescriptionOfCommand(OBJECT caller, OBJECT[] parameters);
+   OBJECT IMETHOD_getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters);
+   OBJECT IMETHOD_maybeParsingActionForCommandStringIfValidDainaCommand(OBJECT caller, OBJECT[] parameters);
+   OBJECT IMETHOD_addValidDainaCommandForCommandStringAndParsingActionAndDescriptionOfCommand(OBJECT caller, OBJECT[] parameters);
 }
 class CLASSIMP_ValidDainaCommandsToParse : CLASSTYPE_ValidDainaCommandsToParse {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
-   static OBJECT emptyListOfValidDainaCommandsToParse(OBJECT caller, OBJECT[] parameters) {
+   private OBJECT IOBJECT_mappingOfValidCommandStringToParsingAction = null;
+   private OBJECT IOBJECT_usageDescriptionOfAllValidCommands = null;
+   static OBJECT CMETHOD_emptyListOfValidDainaCommandsToParse(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   override OBJECT getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters) {
+   override OBJECT IMETHOD_getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   override OBJECT maybeParsingActionForCommandStringIfValidDainaCommand(OBJECT caller, OBJECT[] parameters) {
+   override OBJECT IMETHOD_maybeParsingActionForCommandStringIfValidDainaCommand(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   override OBJECT addValidDainaCommandForCommandStringAndParsingActionAndDescriptionOfCommand(OBJECT caller, OBJECT[] parameters) {
+   override OBJECT IMETHOD_addValidDainaCommandForCommandStringAndParsingActionAndDescriptionOfCommand(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
 }
 
 /* *** CLASS: DainaCommandParsingAction *** */
 interface CLASSTYPE_DainaCommandParsingAction : OBJECT, LAMBDA, CLASSTYPE_Object {
-   OBJECT performActionWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters);
+   OBJECT IMETHOD_performActionWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters);
 }
 class CLASSIMP_DainaCommandParsingAction : CLASSTYPE_DainaCommandParsingAction {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
-   static OBJECT newParsingActionUsingParsingActionToParseCommandWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters) {
+   private OBJECT IOBJECT_parsingActionToParseCommandWithCommandStringSegmentProviderAndThenSendCommandToReceiver = null;
+   static OBJECT CMETHOD_newParsingActionUsingParsingActionToParseCommandWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   override OBJECT performActionWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters) {
+   override OBJECT IMETHOD_performActionWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT parseCommandToParseDainaSourceFileInExposedNamespace(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT CMETHOD_parseCommandToParseDainaSourceFileInExposedNamespace(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT parseCommandToParseDainaSourceFileInExposedAndPrivateNamespace(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT CMETHOD_parseCommandToParseDainaSourceFileInExposedAndPrivateNamespace(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT parseCommandToParseDainaSourceFileInPrivateNamespace(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT CMETHOD_parseCommandToParseDainaSourceFileInPrivateNamespace(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT parseCommandToAnalyseAndSynthesiseSyntaxTree(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT CMETHOD_parseCommandToAnalyseAndSynthesiseSyntaxTree(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT parseCommandToCompileIntoJavaWithinDirectory(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT CMETHOD_parseCommandToCompileIntoJavaWithinDirectory(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT parseCommandToCompileIntoCWithinDirectory(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT CMETHOD_parseCommandToCompileIntoCWithinDirectory(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
-   static OBJECT parseInvalidCommand(OBJECT caller, OBJECT[] parameters) {
+   static OBJECT CMETHOD_parseInvalidCommand(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
 }
