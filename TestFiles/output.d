5cf324540939d75cf342335518ad91a0a57f4010
Samuel Whitton <whittonsamuel@gmail.com>
2025-01-19 19:32:02 +1100

    Stage one bootstrapped compiler ...

--- TestFiles/output.d
+++ TestFiles/output.d
@@ -16,7 +16,7 @@ class ENTRY_POINT_CLASS {
 interface CLASSTYPE_Object : OBJECT, LAMBDA {
    OBJECT IMETHOD_asdf(OBJECT caller, OBJECT[] parameters);
 }
-class CLASSIMP_Object : CLASSTYPE_Object {
+class CLASSIMPL_Object : CLASSTYPE_Object {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
    override OBJECT IMETHOD_asdf(OBJECT caller, OBJECT[] parameters) {
       return caller;
@@ -28,9 +28,11 @@ interface CLASSTYPE_DainaCommandParser : OBJECT, LAMBDA, CLASSTYPE_Object {
    OBJECT IMETHOD_parseNextCommandAndSendToReceiverWithCommandStringSegmentProvider(OBJECT caller, OBJECT[] parameters);
    OBJECT IMETHOD_getUsageDescriptionOfValidDainaCommands(OBJECT caller, OBJECT[] parameters);
 }
-class CLASSIMP_DainaCommandParser : CLASSTYPE_DainaCommandParser {
+class CLASSIMPL_DainaCommandParser : CLASSTYPE_DainaCommandParser {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
+   private CLASSTYPE_Object PARENT_1 = null;
    private OBJECT IOBJECT_validDainaCommandsToParse = null;
+   override OBJECT IMETHOD_asdf(OBJECT caller, OBJECT[] parameters) { return PARENT_1.IMETHOD_asdf(caller, parameters); }
    static OBJECT CMETHOD_newDainaCommandParser(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
@@ -45,7 +47,7 @@ class CLASSIMP_DainaCommandParser : CLASSTYPE_DainaCommandParser {
 /* *** CLASS: DainaCommandStrings *** */
 interface CLASSTYPE_DainaCommandStrings : OBJECT, LAMBDA {
 }
-class CLASSIMP_DainaCommandStrings : CLASSTYPE_DainaCommandStrings {
+class CLASSIMPL_DainaCommandStrings : CLASSTYPE_DainaCommandStrings {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
    static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
@@ -70,7 +72,7 @@ class CLASSIMP_DainaCommandStrings : CLASSTYPE_DainaCommandStrings {
 /* *** CLASS: DainaCommandDescriptionStrings *** */
 interface CLASSTYPE_DainaCommandDescriptionStrings : OBJECT, LAMBDA {
 }
-class CLASSIMP_DainaCommandDescriptionStrings : CLASSTYPE_DainaCommandDescriptionStrings {
+class CLASSIMPL_DainaCommandDescriptionStrings : CLASSTYPE_DainaCommandDescriptionStrings {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
    static OBJECT TMETHOD_PARSE_DAINA_SOURCE_FILE_IN_EXPOSED_NAMESPACE(OBJECT caller, OBJECT[] parameters) {
       return caller;
@@ -98,10 +100,12 @@ interface CLASSTYPE_ValidDainaCommandsToParse : OBJECT, LAMBDA, CLASSTYPE_Object
    OBJECT IMETHOD_maybeParsingActionForCommandStringIfValidDainaCommand(OBJECT caller, OBJECT[] parameters);
    OBJECT IMETHOD_addValidDainaCommandForCommandStringAndParsingActionAndDescriptionOfCommand(OBJECT caller, OBJECT[] parameters);
 }
-class CLASSIMP_ValidDainaCommandsToParse : CLASSTYPE_ValidDainaCommandsToParse {
+class CLASSIMPL_ValidDainaCommandsToParse : CLASSTYPE_ValidDainaCommandsToParse {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
+   private CLASSTYPE_Object PARENT_1 = null;
    private OBJECT IOBJECT_mappingOfValidCommandStringToParsingAction = null;
    private OBJECT IOBJECT_usageDescriptionOfAllValidCommands = null;
+   override OBJECT IMETHOD_asdf(OBJECT caller, OBJECT[] parameters) { return PARENT_1.IMETHOD_asdf(caller, parameters); }
    static OBJECT CMETHOD_emptyListOfValidDainaCommandsToParse(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
@@ -120,9 +124,11 @@ class CLASSIMP_ValidDainaCommandsToParse : CLASSTYPE_ValidDainaCommandsToParse {
 interface CLASSTYPE_DainaCommandParsingAction : OBJECT, LAMBDA, CLASSTYPE_Object {
    OBJECT IMETHOD_performActionWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters);
 }
-class CLASSIMP_DainaCommandParsingAction : CLASSTYPE_DainaCommandParsingAction {
+class CLASSIMPL_DainaCommandParsingAction : CLASSTYPE_DainaCommandParsingAction {
    override OBJECT method(OBJECT caller, OBJECT[] parameters) { return caller; }
+   private CLASSTYPE_Object PARENT_1 = null;
    private OBJECT IOBJECT_parsingActionToParseCommandWithCommandStringSegmentProviderAndThenSendCommandToReceiver = null;
+   override OBJECT IMETHOD_asdf(OBJECT caller, OBJECT[] parameters) { return PARENT_1.IMETHOD_asdf(caller, parameters); }
    static OBJECT CMETHOD_newParsingActionUsingParsingActionToParseCommandWithCommandStringSegmentProviderAndThenSendCommandToReceiver(OBJECT caller, OBJECT[] parameters) {
       return caller;
    }
