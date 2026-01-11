5ac2f17c9cf04c440063c104fa3701cc65214b65
Samuel Whitton <whittonsamuel@gmail.com>
2026-01-11 18:55:08 +1100

    switch compilers

--- TestFiles/run_daina_compilation_test.sh
+++ TestFiles/run_daina_compilation_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./DainaCompiler_Build/Daina < TestFiles/daina_compilation_script.dai-compilation
-dmd TestFiles/output.d -O -of=TestFiles/output
+ldc2 TestFiles/output.d -O -of=TestFiles/output
 #./TestFiles/output
 ./TestFiles/output < TestFiles/daina_compilation_script.dai-compilation
 #daina -output DainaCompiler_Build/Daina -compile $(find $(pwd)/DainaCompiler -type f -not -path '*/\.*' |grep '.dai$')
