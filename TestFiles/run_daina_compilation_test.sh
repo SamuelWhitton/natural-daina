fdbc4e26a078ba9aec333b90c82f5f76a53c6c92
Samuel Whitton <whittonsamuel@gmail.com>
2026-01-26 19:13:35 +1100

    Current state

--- TestFiles/run_daina_compilation_test.sh
+++ TestFiles/run_daina_compilation_test.sh
@@ -2,7 +2,7 @@
 ./DainaCompiler_Build/Daina < TestFiles/daina_compilation_script.dai-compilation
 ldc2 TestFiles/output.d -O -of=TestFiles/output
 #./TestFiles/output
-./TestFiles/output < TestFiles/daina_compilation_script.dai-compilation
+./TestFiles/output < TestFiles/daina_compilation_script_output2.dai-compilation
 #daina -output DainaCompiler_Build/Daina -compile $(find $(pwd)/DainaCompiler -type f -not -path '*/\.*' |grep '.dai$')
 
 
